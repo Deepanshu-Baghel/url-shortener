7777694d1357254a6cb7d9e7862dd3174cbed732
Deepanshu <bagheldeepanshu7451@gmail.com>
2026-02-14 15:58:32 +0530

    first commit

---
+++ url-shortener/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>URL Shortener</h1>
+      <p>Welcome to the URL Shortener app! This application allows you to easily shorten long URLs into more manageable and shareable links. Simply enter a long URL, and we'll generate a shorter version for you to use. Whether you're sharing links on social media, in emails, or anywhere else, our URL shortener makes it easy to share your content without the hassle of long URLs.</p>
+    </div>
+  )
+}
+
+export default App
